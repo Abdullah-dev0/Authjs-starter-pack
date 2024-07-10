39162c1d6be6418bdeebd9c0b9ac6324719a12da
Abdullah-142 <abdulah14200@gmail.com>
2024-07-10 20:52:52 +0500

    refactor: Improve sign out functionality in Setting page

--- app/(protected)/setting/page.tsx
+++ app/(protected)/setting/page.tsx
@@ -1,27 +1,28 @@
+// @ts-ignore
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 
 const Setting = async () => {
-  const session = await auth();
+	const session = await auth();
 
-  const handleSignOut = async () => {
-    "use server";
-    await signOut({
-      redirectTo: "/auth/login",
-    });
-  };
+	const handleSignOut = async () => {
+		"use server";
+		await signOut({
+			redirectTo: "/auth/login",
+		});
+	};
 
-  return (
-    <div>
-      <div>{JSON.stringify(session, null, 2)}</div>
+	return (
+		<div>
+			<div>{JSON.stringify(session, null, 2)}</div>
 
-      <div>
-        <form action={handleSignOut}>
-          <Button type="submit">Sign Out</Button>
-        </form>
-      </div>
-    </div>
-  );
+			<div>
+				<form action={handleSignOut}>
+					<Button type="submit">Sign Out</Button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Setting;
